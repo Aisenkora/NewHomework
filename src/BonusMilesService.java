27a645832609ca456cbd2aa76ac93463b8c3d867
Aesonne <radius.05-good@icloud.com>
2024-04-23 22:53:29 +0300

    Modifided

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+    int miles;
+        if (price > 10_000) {
+        miles = 500;
+        } else {
+        miles = price / 20;
+        }
+
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 15_000;
-        int x = 20;
-        int mile = amount / x;
-
-        System.out.println("Количество полученных миль: " + mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
