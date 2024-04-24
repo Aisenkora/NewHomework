964e3bdacddac320fc537af2359b1b4a158c5623
Aesonne <radius.05-good@icloud.com>
2024-04-24 21:47:08 +0300

    Refinement

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
     public int calculate(int price) {
-    int miles;
+        int miles;
         if (price > 10_000) {
-        miles = 500;
+            miles = 500;
         } else {
-        miles = price / 20;
+            miles = price / 20;
         }
 
         return miles;
